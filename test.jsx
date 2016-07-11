bb63b961411a6a9bf482a2bca68c7f05e27fb620
bgpat <bgpat23@gmail.com>
2016-07-11 19:02:54 +0900

    Add sample code

---
+++ test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TestElement extends React.Component {
+  render() {
+    return (
+      <div>
+        hello, world.
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<TestElement />, document.getElementById('main'));
