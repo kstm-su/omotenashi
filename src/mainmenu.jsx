aa1c1d95c755480dac64426e37b2670dbf1430f0
bgpat <bgpat23@gmail.com>
2016-07-15 11:21:57 +0900

    Add icon

--- src/mainmenu.jsx
+++ src/mainmenu.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import DateRange from 'material-ui/svg-icons/action/date-range.js';
+import Assignment from 'material-ui/svg-icons/action/assignment.js';
 
 export default class MainMenu extends React.Component {
   render() {
@@ -11,8 +13,8 @@ export default class MainMenu extends React.Component {
         open={this.props.show}
         onRequestChange={show => this.props[show ? 'open' : 'close']()}
       >
-        <MenuItem href="#" onTouchTap={this.props.close}>時間割</MenuItem>
-        <MenuItem href="#todo" onTouchTap={this.props.close}>イベント一覧</MenuItem>
+        <MenuItem href="#" leftIcon={<DateRange />} onTouchTap={this.props.close}>時間割</MenuItem>
+        <MenuItem href="#todo" leftIcon={<Assignment />} onTouchTap={this.props.close}>イベント一覧</MenuItem>
       </Drawer>
     );
   }
