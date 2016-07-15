01e18cdb064ac59317804ab424387e19a72cfe7c
Goryudyuma <umaretekyoumade@gmail.com>
2016-07-16 07:06:24 +0900

    インデントの統一

--- src/todo.jsx
+++ src/todo.jsx
@@ -41,18 +41,17 @@ export default class Todo extends React.Component{
 }
 
 class TodoComponent extends React.Component{
-	//class TodoComponent extends TableRow{
   dateFormatting(targetTime){
 	return targetTime.toLocaleString();
   }
 
   remainingTime(targetTime){
-	var time = targetTime.getTime() - new Date().getTime();
-	var minutes = parseInt(time/60000);
-	var hours = parseInt(minutes/60);
-	var days = parseInt(hours/24);
-	if(days > 1) return this.dateFormatting(targetTime);
-	return (days?String(days) + " days ":" ") + (hours%24?String(hours%24) + " hours ":" ") + (minutes%60?String(minutes%60) + " mintues ":" ") 
+    var time = targetTime.getTime() - new Date().getTime();
+    var minutes = parseInt(time/60000);
+    var hours = parseInt(minutes/60);
+    var days = parseInt(hours/24);
+    if(days > 1) return this.dateFormatting(targetTime);
+    return (days?String(days) + " days ":" ") + (hours%24?String(hours%24) + " hours ":" ") + (minutes%60?String(minutes%60) + " mintues ":" ") 
   }
 
   timeUpdate() {
