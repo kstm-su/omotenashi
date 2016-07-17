178288447dd219e9979bda1d22b689b20cb5884d
bgpat <bgpat23@gmail.com>
2016-07-18 02:24:54 +0900

    Fix indent

--- src/course/index.jsx
+++ src/course/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-	Card, 
-	CardTitle, 
-	CardText
+  Card, 
+  CardTitle, 
+  CardText
 } from 'material-ui/Card';
 
 import userData from '../testdata';
@@ -15,22 +15,22 @@ export default class Course extends React.Component {
     this.periods = userData.periods;
   }
 
-	render() {
+  render() {
     let course = this.courses.filter(c => c.id == this.props.params.id).pop();
-		return(
-			<Card>
-				<CardTitle
-					title={course.title}
+    return(
+      <Card>
+        <CardTitle
+          title={course.title}
           subtitle={course.label}
         />
-				<CardText>
+        <CardText>
           {course.schedules.map(schedule => {
             let week = this.weeks[schedule.week];
             let period = this.periods[schedule.period];
             return `${week} ${period}`;
           }).join(', ')}
-				</CardText>
-			</Card>
-		);
-	}
+        </CardText>
+      </Card>
+    );
+  }
 }
