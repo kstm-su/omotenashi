75e96e8fec9d6175c53bdc705a25c4fd25fac411
bgpat <bgpat23@gmail.com>
2016-07-19 13:48:03 +0900

    Update test data
    ふぉのさんの履修情報をテスト用データとして追加 #16

--- src/testdata.js
+++ src/testdata.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   weeks: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
   periods: [1, 2, 3, 4],
   courses: [
@@ -9,74 +9,113 @@ module.exports = {
       schedules: [{
         week: 0,
         period: 0,
-        location: '第2講義室',
+        location: '第1講義室',
       }, {
         week: 1,
         period: 3,
-        location: '第2講義室',
+        location: '第1講義室',
       }],
     }, {
       id: 2000,
+      title: '論理回路Ⅰ',
+      label: '論理回路Ⅰ',
+      schedules: [{
+        week: 0,
+        period: 1,
+        location: '第2講義室',
+      }, {
+        week: 2,
+        period: 2,
+        location: '4F実験室',
+      }],
+    }, {
+      id: 3000,
       title: '画像処理',
       label: '画像処理',
       schedules: [{
         week: 0,
-        period: 1,
-        location: '第2講義室',
+        period: 2,
+        location: '第1講義室',
       }, {
         week: 1,
         period: 1,
-        location: '第2講義室',
+        location: '第1講義室',
       }],
     }, {
-      id: 3000,
+      id: 4000,
       title: 'ヒューマンコンピュータインタラクション',
       label: 'HCI',
       schedules: [{
         week: 0,
-        period: 2,
-        location: '第2講義室',
-      }, {
-        week: 0,
         period: 3,
-        location: '第2講義室',
+        location: '第1講義室',
+      }, {
+        week: 1,
+        period: 2,
+        location: '第1講義室',
       }],
     }, {
-      id: 4000,
+      id: 5000,
       title: 'プログラミング言語論',
       label: 'プロ言論',
       schedules: [{
         week: 2,
         period: 3,
-        location: '第2講義室',
+        location: '第1講義室',
       }, {
         week: 3,
         period: 0,
-        location: '第2講義室',
+        location: '第1講義室',
       }],
     }, {
-      id: 5000,
+      id: 6000,
       title: '論理回路Ⅱ',
       label: '論理回路Ⅱ',
       schedules: [{
         week: 3,
         period: 1,
-        location: '第2講義室',
+        location: '第1講義室',
       }, {
         week: 4,
-        period: 3,
+        period: 2,
         location: '4F実験室',
       }],
     }, {
-      id: 6000,
-      title: 'アカデミックイングリッシュ',
-      label: 'AE',
+      id: 7000,
+      title: 'ソフトウェア工学',
+      label: 'ソフ工',
       schedules: [{
-        week: 1,
-        period: 3,
-        location: '物理のどっか',
+        week: 2,
+        period: 1,
+        location: '第1講義室',
+      }, {
+        week: 2,
+        period: 2,
+        location: '第1講義室',
       }],
-    }
+    }, {
+      id: 8000,
+      title: '微分積分学Ⅰ',
+      label: '微積',
+      schedules: [{
+        week: 2,
+        period: 2,
+        location: '第2講義室',
+      }],
+    }, {
+      id: 9000,
+      title: 'ネットワークコンピューティング',
+      label: 'ネトコン',
+      schedules: [{
+        week: 4,
+        period: 0,
+        location: '第1講義室',
+      }, {
+        week: 4,
+        period: 1,
+        location: '第1講義室',
+      }],
+    },
   ],
   events: [
     {
