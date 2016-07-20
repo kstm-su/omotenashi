99c5d140d3a21f7bf3461a9e5e976a88cc50c444
Goryudyuma <umaretekyoumade@gmail.com>
2016-07-21 06:30:21 +0900

    appの分離

---
+++ src/main/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppRouter from './router';
+
+export default class App extends React.Component {
+  render() {
+    return <AppRouter />;
+  }
+}
--- src/main/index.jsx
+++ src/main/index.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import AppRouter from './router';
+import App from './app';
 
 import { Provider } from 'react-redux';
 import configureStore from '../redux/store'
 
 const store = configureStore();
 
-class App extends React.Component {
-  render() {
-    return <AppRouter />;
-  }
-}
 
 injectTapEventPlugin();
 ReactDOM.render(
