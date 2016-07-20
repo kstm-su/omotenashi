e6ff37640dbf0fe1c28ad6d716cc13deefb6fc0d
bgpat <bgpat23@gmail.com>
2016-07-20 13:58:13 +0900

    Change style of course page

--- src/course/index.jsx
+++ src/course/index.jsx
@@ -1,42 +1,106 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Chip, Avatar} from 'material-ui';
 import {
-  Card, 
-  CardTitle, 
-  CardText
-} from 'material-ui/Card';
+  ActionDateRange,
+  ActionEvent,
+  SocialPerson,
+  MapsMyLocation,
+} from 'material-ui/svg-icons';
 
 import userData from '../testdata';
 
-export default class Course extends React.Component {
-  constructor(props) {
-    super(props);
-    this.courses = userData.courses;
-    this.weeks = userData.weeks;
-    this.periods = userData.periods;
-  }
+export default class Course extends Component {
+  static title = '授業情報';
 
-  static title(params) {
-    /* TODO: userData置き換える */
-    let course = userData.courses.filter(c => c.id == params.id)[0];
-    return course.title;
+  componentWillMount() {
+    this.setState(userData);
   }
 
   render() {
-    let course = this.courses.filter(c => c.id == this.props.params.id).pop();
-    return(
-      <Card>
-        <CardTitle
-          title={course.title}
-          subtitle={course.label}
-        />
-        <CardText>
-          {course.schedules.map(schedule => {
-            let week = this.weeks[schedule.week];
-            let period = this.periods[schedule.period];
-            return `${week} ${period}`;
-          }).join(', ')}
-        </CardText>
-      </Card>
+    let course = this.state.courses.filter(c => {
+      return c.id == this.props.params.id;
+    }).shift();
+    return (
+      <div>
+        <CourseOverview data={this.state} course={course} />
+      </div>
+    );
+  }
+}
+
+class CourseOverview extends Component {
+  labelStyle = {
+    display: 'block',
+    color: '#999',
+  };
+
+  chipContainerStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+  };
+
+  componentWillMount() {
+    this.setState(this.props.data);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>
+          {this.props.course.title}
+          <small style={this.labelStyle}>
+            {this.props.course.label}
+          </small>
+        </h2>
+        <div style={this.chipContainerStyle}>
+          {this.props.course.instructors.map((instructor, i) => {
+            return <AvatarChip
+              key={i}
+              label={instructor.name}
+              icon={instructor.image || <SocialPerson />}
+            />;
+          })}
+          {this.props.course.schedules.map((schedule, i) => {
+            let week = this.state.weeks[schedule.week];
+            let period = this.state.periods[schedule.period];
+            return <AvatarChip
+              key={i}
+              label={`${week} ${period}`}
+              icon={<ActionEvent />}
+            />;
+          })}
+          {this.props.course.schedules.map(schedule => {
+            return schedule.location;
+          }).filter((e, i, a) => a.indexOf(e) === i).map((location, i) => (
+            <AvatarChip
+              key={i}
+              label={location}
+              icon={<MapsMyLocation />}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+class AvatarChip extends Component {
+  style = {
+    margin: 4,
+  };
+
+  render() {
+    let props = {};
+    if (typeof this.props.icon === 'string') {
+      props.src = this.props.icon;
+    } else {
+      props.icon = this.props.icon;
+    }
+    return (
+      <Chip onTouchTap={() => null} style={this.style}>
+        <Avatar {...props} />
+        {this.props.label}
+      </Chip>
     );
   }
 }
